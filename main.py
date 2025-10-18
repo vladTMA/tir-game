bf4aeadad9303b594b0350949f7cf2f2ac225ffc
vladTMA <vlad_t54@yahoo.com>
2025-10-18 18:23:58 +0300

    аdd cons code

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 # main.py
 # game tir
+from random import random
+from turtledemo.nim import SCREENWIDTH
 
 import pygame
 from pygame.examples.go_over_there import running
+import random
 
 pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("img/icon.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target_bullseye.png")
+target_width = 80
+target_height = 80
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
 
 running = True
 while running:
@@ -14,3 +32,4 @@ while running:
 
 
 pygame.quit()
+
