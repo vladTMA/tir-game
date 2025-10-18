846cb387a5268c90407b6a1d0e845ea316833877
vladTMA <vlad_t54@yahoo.com>
2025-10-18 20:55:50 +0300

    add сycle сode

--- main.py
+++ main.py
@@ -1,34 +1,61 @@
 # main.py
 # game tir
-from random import random
-from turtledemo.nim import SCREENWIDTH
-
 import pygame
-from pygame.examples.go_over_there import running
 import random
 
 pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("img/icon.jpg")
+
+# Загружаем иконку
+icon = pygame.image.load("image/images.png")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target_bullseye.png")
+# Загружаем изображение мишени
+original_img = pygame.image.load("image/images.png")
 target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+target_img = pygame.transform.scale(original_img, (target_width, target_height))
 
+# Загружаем звук
+pygame.mixer.init()
+hit_sound = pygame.mixer.Sound("sound/sound_tir.wav")
+
+# Цвет фона
 color = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+score = 0
+font = pygame.font.SysFont(None, 36)
+
+clock = pygame.time.Clock()
 
 running = True
 while running:
-        pass
+        screen.fill(color)
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+               mouse_x, mouse_y = pygame.mouse.get_pos()
+               if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                   hit_sound.play()  # при попадании
+                   target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                   target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                   print(f"Попадание! Новые координаты: ({target_x}, {target_y})")
+                   score += 1
+
+        screen.blit(target_img, (target_x, target_y))
+        score_text = font.render(f"Очки: {score}", True, (255, 255, 255))
+        screen.blit(score_text, (10, 10))
+
+        pygame.display.update()
+        clock.tick(60)
 
 
 pygame.quit()
